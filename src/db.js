0975c074366f28a2c835a9eedc30d29122053b4d
x1vaan <ivanx467@gmail.com>
2022-10-18 18:30:20 -0300

    database connected

---
+++ src/db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+require('dotenv').config()
+
+const {MONGO_URI} = process.env
+
+mongoose.connect(MONGO_URI)
+.then(() => console.log('Database Connected'))
+
+module.exports = mongoose

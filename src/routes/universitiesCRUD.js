aab442f6d7401c3e973417c09a457aa711f6bd07
x1vaan <ivanx467@gmail.com>
2022-10-20 02:10:18 -0300

    routes/get universities, get universities by id and update university

--- src/routes/universitiesCRUD.js
+++ src/routes/universitiesCRUD.js
@@ -6,13 +6,24 @@ const router = Router();
 router.get('/universities', async (req,res) => {
     try {
         const {country} = req.query
+        const {page} = req.query
         if(country){
           const universitiesFilteredByCountry = await University.find({country: country}).select('_id name country state-province')
          return res.status(200).json(universitiesFilteredByCountry)
         }
+        const totalUniversities = await University.count();
+        const totalPages = Math.ceil(totalUniversities / 20) 
 
-       const list = await University.find().skip().limit()
+        if(page > totalPages) {
+            const list = await University.find().skip(totalPages).limit(20)
+            return res.status(200).json({list, information: {totalUniversities,totalPages, currentPage: totalPages}})
+        }
 
+        let skipIndex = page? page * 20 : 1 * 20
+        let index = skipIndex - 20
+       const list = await University.find().skip(index).limit(20)
+
+      res.status(200).json({list, information: {totalUniversities, totalPages,currentPage: page}});
     } catch (error) {
         res.status(500).send(error.message)
     }
@@ -21,8 +32,26 @@ router.get('/universities', async (req,res) => {
 router.get('/universities/:id', async(req,res) =>{
     try {
         const _id  = req.params.id
-       const university = await University.findOne({_id})
+       const university = await University.findById(_id)
        res.status(200).json(university)  
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+});
+
+
+router.put('/universities/:id', async (req,res)=>{
+    try {
+        const _id = req.params.id
+        const {name, domains, web_pages} = req.body
+        await University.updateOne({_id : _id},{
+           name : name,
+           $push: {
+               domains: domains,
+               web_pages: web_pages
+           }
+        })
+         res.status(200).send('University updated succesfully')
     } catch (error) {
         res.status(500).send(error.message)
     }
